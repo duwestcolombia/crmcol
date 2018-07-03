be8b75c5f7db254081ae96956cdc4eeb3e4c686b
David Zambrano <david.zambrano@duwest.com>
2018-07-03 10:37:17 -0500

    Se agrega la validacion por estado de usuario, denegando el acceso a usuarios inactivos

--- php/validar.php
+++ php/validar.php
@@ -14,115 +14,54 @@ $resultado=$db->query("SELECT * FROM usuario WHERE sesion_usuario='$usuario'");
 
 if ($resultado->num_rows > 0) {
 	foreach ($resultado as $res) {
-		if ($res['password'] == $password) {
-			
-			switch ($res['id_rol']) {
-							case 0:
-								//echo '<script>alert("Bienvenido '.$usuenc.'")</script>';
-								$_SESSION['usuario_sesion'] =$res['id_usuario'];
-								$_SESSION['password_sesion']=$res['password'];
-								$_SESSION['tipusuario_sesion']=$res['id_rol'];
-								$_SESSION['zona_sesion']=$res['id_zona'];
-								$usuok=$_SESSION['usuario_sesion'];
-								date_default_timezone_set("America/Bogota");
-								$hoy=date("Y-m-d h:i:sa");
-								$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
-								if ($reglog) {
-									header("location:../superuser/dashboard/index.php");
-									//echo "<script>location.href='../repvisitas.php'</script>";
-								}
-								else
-								{
-									echo $dbaudi->error;
-								}
-										
-							break;
-							case 1:
-								//echo '<script>alert("Bienvenido '.$usuenc.'")</script>';
-								$_SESSION['usuario_sesion'] =$res['id_usuario'];
-								$_SESSION['password_sesion']=$res['password'];
-								$_SESSION['tipusuario_sesion']=$res['id_rol'];
-								$_SESSION['zona_sesion']=$res['id_zona'];
-								$usuok=$_SESSION['usuario_sesion'];
-								date_default_timezone_set("America/Bogota");
-								$hoy=date("Y-m-d h:i:sa");
-								$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
-								if ($reglog) {
-									header("location:../superuser/dashboard/index.php");
-									//echo "<script>location.href='../repvisitas.php'</script>";
-								}
-								else
-								{
-									echo $dbaudi->error;
-								}
-										
-							break;
-							case 2:
-							//VALIDA LIDER CON UN TERRITORIO
-								//echo '<script>alert("Bienvenido Lider territorio")</script>';
-								$_SESSION['usuario_sesion'] =$res['id_usuario'];
-								$_SESSION['password_sesion']=$res['password'];
-								$_SESSION['tipusuario_sesion']=$res['id_rol'];
-								$_SESSION['zona_sesion']=$res['id_zona'];
-								$usuok=$_SESSION['usuario_sesion'];
-								date_default_timezone_set("America/Bogota");
-								$hoy=date("Y-m-d h:i:sa");
-								$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
-								if ($reglog) {
-									header("location:../admin/dashboard/index.php");
-									//echo "<script>location.href='../repvisitas.php'</script>";
-								}
-								else
-								{
-									echo $dbaudi->error;
-								}
-								//echo "<script>location.href='../repvisitas.php'</script>";
+		if($res['estado_usuario'] =="ACTIVO"){
+
+		
+			if ($res['password'] == $password) {
 				
-							break;
-							case 3:
-							//VALIDA LIDER BOYACA Y CUNDINAMARCA
-								//echo '<script>alert("Bienvenido Lider territorio")</script>';
-								$_SESSION['usuario_sesion'] =$res['id_usuario'];
-								$_SESSION['password_sesion']=$res['password'];
-								$_SESSION['tipusuario_sesion']=$res['id_rol'];
-								$_SESSION['zona_sesion']=$res['id_zona'];
-								$usuok=$_SESSION['usuario_sesion'];
-								date_default_timezone_set("America/Bogota");
-								$hoy=date("Y-m-d h:i:sa");
-								$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
-								if ($reglog) {
-									header("location:../admin/dashboard/index.php");
-									//echo "<script>location.href='../repvisitas.php'</script>";
-								}
-								else
-								{
-									echo $dbaudi->error;
-								}
-								//echo "<script>location.href='../repvisitas.php'</script>";
-							break;
-							case 4:
-							//VALIDA LIDER FLORES SABANA Y FLORES ANTIOQUIA
-								//echo '<script>alert("Bienvenido Lider territorio")</script>';
-								$_SESSION['usuario_sesion'] =$res['id_usuario'];
-								$_SESSION['password_sesion']=$res['password'];
-								$_SESSION['tipusuario_sesion']=$res['id_rol'];
-								$_SESSION['zona_sesion']=$res['id_zona'];
-								$usuok=$_SESSION['usuario_sesion'];
-								date_default_timezone_set("America/Bogota");
-								$hoy=date("Y-m-d h:i:sa");
-								$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
-								if ($reglog) {
-									header("location:../admin/dashboard/index.php");
-									//echo "<script>location.href='../repvisitas.php'</script>";
-								}
-								else
-								{
-									echo $dbaudi->error;
-								}
-								//echo "<script>location.href='../repvisitas.php'</script>";
-							break;	
-							case 5:
-								//VALIDA LIDER NARIÑO CAUCA Y HUILA
+				switch ($res['id_rol']) {
+								case 0:
+									//echo '<script>alert("Bienvenido '.$usuenc.'")</script>';
+									$_SESSION['usuario_sesion'] =$res['id_usuario'];
+									$_SESSION['password_sesion']=$res['password'];
+									$_SESSION['tipusuario_sesion']=$res['id_rol'];
+									$_SESSION['zona_sesion']=$res['id_zona'];
+									$usuok=$_SESSION['usuario_sesion'];
+									date_default_timezone_set("America/Bogota");
+									$hoy=date("Y-m-d h:i:sa");
+									$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
+									if ($reglog) {
+										header("location:../superuser/dashboard/index.php");
+										//echo "<script>location.href='../repvisitas.php'</script>";
+									}
+									else
+									{
+										echo $dbaudi->error;
+									}
+											
+								break;
+								case 1:
+									//echo '<script>alert("Bienvenido '.$usuenc.'")</script>';
+									$_SESSION['usuario_sesion'] =$res['id_usuario'];
+									$_SESSION['password_sesion']=$res['password'];
+									$_SESSION['tipusuario_sesion']=$res['id_rol'];
+									$_SESSION['zona_sesion']=$res['id_zona'];
+									$usuok=$_SESSION['usuario_sesion'];
+									date_default_timezone_set("America/Bogota");
+									$hoy=date("Y-m-d h:i:sa");
+									$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
+									if ($reglog) {
+										header("location:../superuser/dashboard/index.php");
+										//echo "<script>location.href='../repvisitas.php'</script>";
+									}
+									else
+									{
+										echo $dbaudi->error;
+									}
+											
+								break;
+								case 2:
+								//VALIDA LIDER CON UN TERRITORIO
 									//echo '<script>alert("Bienvenido Lider territorio")</script>';
 									$_SESSION['usuario_sesion'] =$res['id_usuario'];
 									$_SESSION['password_sesion']=$res['password'];
@@ -141,55 +80,122 @@ if ($resultado->num_rows > 0) {
 										echo $dbaudi->error;
 									}
 									//echo "<script>location.href='../repvisitas.php'</script>";
-							break;
-							case 6:
-								//echo '<script>alert("Bienvenido Representante tecnico")</script>';
-								$_SESSION['usuario_sesion'] =$res['id_usuario'];
-								$_SESSION['password_sesion']=$res['password'];
-								$usuok=$_SESSION['usuario_sesion'];
-								date_default_timezone_set("America/Bogota");
-								$hoy=date("Y-m-d h:i:sa");
-								$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
-								if ($reglog) {
-									header("location:../user/repvisitas.php");
+					
+								break;
+								case 3:
+								//VALIDA LIDER BOYACA Y CUNDINAMARCA
+									//echo '<script>alert("Bienvenido Lider territorio")</script>';
+									$_SESSION['usuario_sesion'] =$res['id_usuario'];
+									$_SESSION['password_sesion']=$res['password'];
+									$_SESSION['tipusuario_sesion']=$res['id_rol'];
+									$_SESSION['zona_sesion']=$res['id_zona'];
+									$usuok=$_SESSION['usuario_sesion'];
+									date_default_timezone_set("America/Bogota");
+									$hoy=date("Y-m-d h:i:sa");
+									$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
+									if ($reglog) {
+										header("location:../admin/dashboard/index.php");
+										//echo "<script>location.href='../repvisitas.php'</script>";
+									}
+									else
+									{
+										echo $dbaudi->error;
+									}
 									//echo "<script>location.href='../repvisitas.php'</script>";
-								}
-								else
-								{
-									echo $dbaudi->error;
-								}
-								
-								//echo "<script>location.href='../repvisitas.php'</script>";
-							break;
-							case 7:
-								//echo '<script>alert("Bienvenido Promotor")</script>';
-								$_SESSION['usuario_sesion'] =$res['id_usuario'];
-								$_SESSION['password_sesion']=$res['password'];
-								$usuok=$_SESSION['usuario_sesion'];
-								date_default_timezone_set("America/Bogota");
-								$hoy=date("Y-m-d h:i:sa");
-								$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
-								if ($reglog) {
-									header("location:../user/repvisitas.php");
+								break;
+								case 4:
+								//VALIDA LIDER FLORES SABANA Y FLORES ANTIOQUIA
+									//echo '<script>alert("Bienvenido Lider territorio")</script>';
+									$_SESSION['usuario_sesion'] =$res['id_usuario'];
+									$_SESSION['password_sesion']=$res['password'];
+									$_SESSION['tipusuario_sesion']=$res['id_rol'];
+									$_SESSION['zona_sesion']=$res['id_zona'];
+									$usuok=$_SESSION['usuario_sesion'];
+									date_default_timezone_set("America/Bogota");
+									$hoy=date("Y-m-d h:i:sa");
+									$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
+									if ($reglog) {
+										header("location:../admin/dashboard/index.php");
+										//echo "<script>location.href='../repvisitas.php'</script>";
+									}
+									else
+									{
+										echo $dbaudi->error;
+									}
 									//echo "<script>location.href='../repvisitas.php'</script>";
-								}
-								else
-								{
-									echo $dbaudi->error;
-								}
-								
-								//echo "<script>location.href='../repvisitas.php'</script>";
-							break;
-						}
-			
+								break;	
+								case 5:
+									//VALIDA LIDER NARIÑO CAUCA Y HUILA
+										//echo '<script>alert("Bienvenido Lider territorio")</script>';
+										$_SESSION['usuario_sesion'] =$res['id_usuario'];
+										$_SESSION['password_sesion']=$res['password'];
+										$_SESSION['tipusuario_sesion']=$res['id_rol'];
+										$_SESSION['zona_sesion']=$res['id_zona'];
+										$usuok=$_SESSION['usuario_sesion'];
+										date_default_timezone_set("America/Bogota");
+										$hoy=date("Y-m-d h:i:sa");
+										$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
+										if ($reglog) {
+											header("location:../admin/dashboard/index.php");
+											//echo "<script>location.href='../repvisitas.php'</script>";
+										}
+										else
+										{
+											echo $dbaudi->error;
+										}
+										//echo "<script>location.href='../repvisitas.php'</script>";
+								break;
+								case 6:
+									//echo '<script>alert("Bienvenido Representante tecnico")</script>';
+									$_SESSION['usuario_sesion'] =$res['id_usuario'];
+									$_SESSION['password_sesion']=$res['password'];
+									$usuok=$_SESSION['usuario_sesion'];
+									date_default_timezone_set("America/Bogota");
+									$hoy=date("Y-m-d h:i:sa");
+									$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
+									if ($reglog) {
+										header("location:../user/repvisitas.php");
+										//echo "<script>location.href='../repvisitas.php'</script>";
+									}
+									else
+									{
+										echo $dbaudi->error;
+									}
+									
+									//echo "<script>location.href='../repvisitas.php'</script>";
+								break;
+								case 7:
+									//echo '<script>alert("Bienvenido Promotor")</script>';
+									$_SESSION['usuario_sesion'] =$res['id_usuario'];
+									$_SESSION['password_sesion']=$res['password'];
+									$usuok=$_SESSION['usuario_sesion'];
+									date_default_timezone_set("America/Bogota");
+									$hoy=date("Y-m-d h:i:sa");
+									$reglog=$dbaudi->query("INSERT Into log(id_log,nom_log,fecha_log,id_usuario) values(NULL,'Ingreso al sistema','$hoy','$usuok')")or die($dbaudi->error);
+									if ($reglog) {
+										header("location:../user/repvisitas.php");
+										//echo "<script>location.href='../repvisitas.php'</script>";
+									}
+									else
+									{
+										echo $dbaudi->error;
+									}
+									
+									//echo "<script>location.href='../repvisitas.php'</script>";
+								break;
+							}
+				
 
 
-		} else {
-			echo '<script>alert("Contraseña Incorrecta")</script>';
-			echo "<script>location.href='../ingreso.php'</script>";
-		}
+			} else {
+				echo '<script>alert("Contraseña Incorrecta")</script>';
+				echo "<script>location.href='../ingreso.php'</script>";
+			}
 		
-
+		}else{
+			echo '<script>alert("Su usuario no esta activo en nuestra plataforma, comuniquese con el administrador")</script>';
+				echo "<script>location.href='../ingreso.php'</script>";
+		}
 
 	}
 } else {
